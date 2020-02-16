e6c325e525798ff64e3c76a77d3938838feb8d3c
ChanceDurr <chancedurr@gmail.com>
2020-02-15 22:45:36 -0800

    jitterless code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Raspberry Pi projects
+
+## Useful links
+- https://steemit.com/python/@makerhacks/jitter-free-servo-control-on-the-raspberry-pi
--- servo_control.py
+++ servo_control.py
@@ -1,66 +1,15 @@
-import gpiozero
-from gpiozero import Servo
 from time import sleep
-from gpiozero.tools import sin_values
-import RPi.GPIO as GPIO
+import pigpio
 
-# GPIO.cleanup()
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setwarnings(False)
+pi = pigpio.pi()
 
-# GPIO.setup(11, GPIO.OUT)
-# servo = GPIO.PWM(11, 50)
-
-# servo.start(0)
-# sleep(2)
-
-# duty = 0
-
-# while duty <= 12:
-#     servo.ChangeDutyCycle(duty)
-#     sleep(1)
-#     duty += 1
-
-# sleep(2)
-
-# servo.ChangeDutyCycle(7)
-
-# sleep(2)
-
-# servo.ChangeDutyCycle(2)
-# sleep(.5)
-# servo.ChangeDutyCycle(0)
-
-# servo.stop()
-# GPIO.cleanup()
-
-#####################
-
-myGPIO=12
- 
-myCorrection=0.45
-maxPW=(2.0+myCorrection)/1000
-minPW=(1.0-myCorrection)/1000
- 
-servo = Servo(myGPIO,min_pulse_width=minPW,max_pulse_width=maxPW)
- 
 while True:
-    servo.mid()
-    print("mid")
-    sleep(0.5)
-    servo.min()
-    print("min")
+
+    pi.set_servo_pulsewidth(12, 0)    # off
     sleep(1)
-    servo.mid()
-    print("mid")
-    sleep(0.5)
-    servo.max()
-    print("max")
+    pi.set_servo_pulsewidth(12, 550) # position anti-clockwise
     sleep(1)
-
-###############
-
-# servo = Servo(12)
-
-# servo.source = sin_values()
-# servo.source_delay = 0.1
+    pi.set_servo_pulsewidth(12, 1425) # middle
+    sleep(1)
+    pi.set_servo_pulsewidth(12, 2300) # position clockwise
+    sleep(1)
