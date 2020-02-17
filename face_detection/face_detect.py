bca32f542f8ed06b5a93e997dbd7105f1b517d0f
ChanceDurr <chancedurr@gmail.com>
2020-02-16 19:42:11 -0800

    removed pipfile

---
+++ face_detection/face_detect.py
@@ -0,0 +1,47 @@
+
+import cv2
+import sys
+import os
+
+os.system("export DISPLAY=:0.0")
+
+cascPath = 'haarcascade_frontalface_default.xml'
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    width = int(video_capture.get(3))
+    height = int(video_capture.get(4))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=6,
+        minSize=(80, 80),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+
+    # Create a boundaryof where we want the face
+    cv2.rectangle(frame, (250, 0), (width-250, height), (255, 0, 0), 2)
+
+
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
